3131dde3e60bf694770d144ca819b017d1205b24
Manju B S <manjubscse@gmail.com>
2017-11-08 18:28:46 +0530

    Assignment8.3

---
+++ app/src/main/java/com/manjubs/NamesWithGridView/MainActivity.java
@@ -0,0 +1,37 @@
+package com.manjubs.NamesWithGridView;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    // Create Object of GridView And Array List Holds Image Enums
+    GridView imageHolderGridView;
+    ArrayList<Integer> androidVersionList;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Type Cast GridView From Activity
+        imageHolderGridView = (GridView) findViewById(R.id.imageHolderGridView);
+
+        // Add Android Version Images to Array List.
+
+        androidVersionList = new ArrayList<Integer>();
+        androidVersionList.add(R.drawable.gingerbread);
+        androidVersionList.add(R.drawable.honeycomb);
+        androidVersionList.add(R.drawable.icecreamsandwich);
+        androidVersionList.add(R.drawable.jellybean);
+        androidVersionList.add(R.drawable.kitkat);
+        androidVersionList.add(R.drawable.lollipop);
+
+        // Create Object of Image Adapter and Set Adapter to GridView
+        ImageAdapter androidVersionsAdapter = new ImageAdapter(MainActivity.this,androidVersionList);
+        imageHolderGridView.setAdapter(androidVersionsAdapter);
+
+    }
+}
